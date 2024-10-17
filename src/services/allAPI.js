76471820a0a6c115135fcceb7fff9840ace6a2fc
AlbinJames56 <albinjames56@gmail.com>
2024-10-17 15:27:27 +0530

    created api call

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -1,39 +1,45 @@
-import { commonAPI } from "./commonAPI"
-import { SERVER_URL } from "./serverUrl"
+import { commonAPI } from "./commonAPI";
+import { SERVER_URL } from "./serverUrl";
 
 // register Api
-export const  registerAPI =async(user)=>{
-    return await commonAPI("POST",`${SERVER_URL}/register`,user,"")
-}
+export const registerAPI = async (user) => {
+  return await commonAPI("POST", `${SERVER_URL}/register`, user, "");
+};
 // login API
-export const  loginAPI=async(user)=>{
-    return await commonAPI("POST",`${SERVER_URL}/login`,user,"")
-}
+export const loginAPI = async (user) => {
+  return await commonAPI("POST", `${SERVER_URL}/login`, user, "");
+};
 // Add project api
-export const addProjectAPI=async(reqBody,reqHeader)=>{
-    return await commonAPI("POST",`${SERVER_URL}/addProject`,reqBody,reqHeader)
-}
-// // 
+export const addProjectAPI = async (reqBody, reqHeader) => {
+  return await commonAPI(
+    "POST",
+    `${SERVER_URL}/addProject`,
+    reqBody,
+    reqHeader
+  );
+};
+// get home Project Api
+export const getHomeProjectsAPI = async () => {
+  return await commonAPI("GET", `${SERVER_URL}/homeProjects`, "", "");
+};
+// get userProjects
+export const getUserProjectsAPI = async (reqHeader) => {
+  return await commonAPI("GET", `${SERVER_URL}/userProjects`, "", reqHeader);
+};
+// get ALL porjects API
+export const getAllProjectsAPI = async (reqHeader) => {
+  return await commonAPI("GET", `${SERVER_URL}/allProjects`, "", reqHeader);
+};
+
+// //
 // export const =async()=>{
 //     return await commonAPI("",`${SERVER_URL}/`,)
 // }
-// // 
+// //
 // export const =async()=>{
 //     return await commonAPI("",`${SERVER_URL}/`,)
 // }
-// // 
-// export const =async()=>{
-//     return await commonAPI("",`${SERVER_URL}/`,)
-// }
-// // 
-// export const =async()=>{
-//     return await commonAPI("",`${SERVER_URL}/`,)
-// }
-// // 
-// export const =async()=>{
-//     return await commonAPI("",`${SERVER_URL}/`,)
-// }
-// // 
+// //
 // export const =async()=>{
 //     return await commonAPI("",`${SERVER_URL}/`,)
 // }
